08dc9252e6dbc730e0a55238eaea1fe946e949bc
yishai <yishairasowsky@gmail.com>
2021-04-13 19:18:45 +0300

    include script to send image to whatsapp in file of capture

--- capture.py
+++ capture.py
@@ -20,4 +20,29 @@ image = cv2.cvtColor(np.array(image),
    
 # writing it to the disk using opencv
 cv2.imwrite("image1.png", image)
-print('testing')
+
+import os
+from datetime import datetime
+
+from twilio.rest import Client
+
+
+account_sid = os.environ['TWILIO_ACCOUNT_SID']
+auth_token = os.environ['TWILIO_AUTH_TOKEN']
+
+client = Client(account_sid, auth_token)
+ 
+curr_time = str(datetime.now()).split()[1][:8]  
+
+message = client.messages.create( 
+    from_=f'whatsapp:+14155238886',
+    
+    body=f"Here my Yishai's current screenshot, photographed and sent to Shira at {curr_time}",      
+    
+    media_url=["https://6c89d8b24f96.ngrok.io/image1.png"],
+
+
+    to=f"whatsapp:+{os.environ['PHONE_NUMBER']}"
+                          ) 
+ 
+print(message.sid)
