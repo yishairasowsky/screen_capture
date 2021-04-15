f6188ab681fa2ed0d55de0014e20b0184de6655f
yishai <yishairasowsky@gmail.com>
2021-04-15 19:05:50 +0300

    simplify and gernalize twilio example

--- twilio_example.py
+++ twilio_example.py
@@ -7,21 +7,18 @@ import pyautogui
 from pyngrok import ngrok
 from datetime import datetime
 from twilio.rest import Client
-from private_info import PHONE_NUMBER,TWILIO_ACCOUNT_SID,TWILIO_AUTH_TOKEN
+from private_info import PHONE_NUMBER,TWILIO_ACCOUNT_SID,TWILIO_AUTH_TOKEN, TWILIO_NUMBER
 
 account_sid = TWILIO_ACCOUNT_SID
 auth_token = TWILIO_AUTH_TOKEN
 
 client = Client(account_sid, auth_token)
 
-curr_time = str(datetime.now()).split()[1][:8]  
+message = client.messages \
+                .create(
+                     body="Join Earth's mightiest heroes. Like Kevin Bacon.",
+                     from_=f'+{TWILIO_NUMBER}',
+                     to=f'+{PHONE_NUMBER}'
+                 )
 
-
-message = client.messages.create( 
-    from_=f'whatsapp:+14155238886',
-    body=f"Yishai's computer screen at {curr_time}.",      
-    # media_url=[img_url],
-    to=f"whatsapp:+{PHONE_NUMBER}",
-    ) 
-
-pass
+print(message.sid)
