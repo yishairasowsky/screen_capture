4fb907bfe0a62c2ff06189966483280a2628cc14
yishairasowsky <yishairasowsky@gmail.com>
2022-07-12 19:22:47 +0300

    modify gmail method

--- email_screen.py
+++ email_screen.py
@@ -1,43 +1,29 @@
 import numpy as np
-import smtplib
 import time
+import yagmail
+from functions import current_time, intervals, capture_image, current_weekday, current_date
+from private_info import sender_email, receiver_emails, sender_email_password
 
-from email import encoders
-from email.mime.base import MIMEBase
-from email.mime.multipart import MIMEMultipart
-from functions import current_time,intervals,image
-from private_info import sender_email, receiver_email, sender_email_password
+def main(usage='test'):
+    low,high = intervals(usage=usage)
+    relative_image_path = 'screen.png'
+    while True:
+        try:
+            time.sleep(np.random.randint(low,high))
+            capture_image(relative_image_path)
+            yag = yagmail.SMTP(user=sender_email, password=sender_email_password)
+            subject = f'Yishai\'s screen {current_weekday()} {current_date()}'
+            yag.send(to=receiver_emails, 
+                    subject=subject, 
+                    contents='See the attached image!',
+                    attachments=relative_image_path
+                    )
+            print(f'Sent at {current_time()}')
+        except Exception as e:
+            print(e)
+            continue
 
-# usage = 'test'
-usage = 'real'
-
-low,high = intervals(usage)
-
-while True:
-    try:
-        time.sleep(np.random.randint(low,high))
-        filename = image('screen.png')
-    except:
-        pass
-
-    message = MIMEMultipart()
-    message["From"] = sender_email
-    message['To'] = receiver_email
-    # message['Subject'] = current_time()
-    message['Subject'] = "Yishai's screen"
-    file = "screen.png"
-    attachment = open(file,'rb')
-    obj = MIMEBase('application','octet-stream')
-    obj.set_payload((attachment).read())
-    encoders.encode_base64(obj)
-    obj.add_header('Content-Disposition',"attachment; filename= "+file)
-    message.attach(obj)
-    my_message = message.as_string()
-    email_session = smtplib.SMTP('smtp.gmail.com',587)
-    email_session.starttls()
-    email_session.login(sender_email,password=sender_email_password)
-    email_session.sendmail(sender_email,receiver_email,my_message)
-    email_session.quit()
-    print(f"MAIL SENT at {current_time()}")
-    pass
-
+if __name__ == "__main__":
+    main(
+        usage='real' # comment out in order to run as test
+        )
--- functions.py
+++ functions.py
@@ -1,17 +1,17 @@
-# View instructions at README.md
+import calendar
 import cv2
 import numpy as np
 import pyautogui
-from pyngrok import ngrok
 from datetime import datetime
-from private_info import PHONE_NUMBER,TWILIO_NUMBER
 
 def current_time():
     return str(datetime.now()).split()[1][:5]
-    
-# def public_url():
-#     http_tunnel = ngrok.connect()
-#     return http_tunnel.public_url
+
+def current_weekday():
+    return calendar.day_name[datetime.now().weekday()]
+
+def current_date():
+    return datetime.now().isoformat()[:10]
 
 def intervals(usage):    
     if usage == 'test':
@@ -23,17 +23,11 @@ def intervals(usage):
         low, high = 60, max_seconds
     return low, high
 
-def image(filename):
-    image = pyautogui.screenshot() # take picture
-    image = cv2.cvtColor(np.array(image),cv2.COLOR_RGB2BGR) # not sure what this does
+def capture_image(filename):
+    image = pyautogui.screenshot()
+    image = cv2.cvtColor(np.array(image),cv2.COLOR_RGB2BGR) # convert from not sure what this does
     cv2.imwrite(filename, image) # store image in file
-    return filename
 
-# def img_url(url_stem,filename):
-#     img_url = f"{url_stem}/{filename}" # create address to access image
-#     return img_url
-
-# def send(client,body,img_url=None):
-#     client.messages.create(
-#         from_=f'whatsapp:+{TWILIO_NUMBER}',to=f"whatsapp:+{PHONE_NUMBER}",
-#         body=body,media_url=[img_url]) 
+if __name__ == "__main__":
+    print(current_date())
+    pass
