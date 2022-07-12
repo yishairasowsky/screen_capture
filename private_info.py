798d496fa47246cd60b6059a96fab50d16281c30
yishairasowsky <yishairasowsky@gmail.com>
2022-07-12 19:27:39 +0300

    show format for private info

--- private_info.py
+++ private_info.py
@@ -1,3 +1,9 @@
-sender_email = "myname@gmail.com"
-sender_email_password = "mypassword"
-receiver_email = "myfriend@gmail.com"
+# create sender_email_password using this link 
+# https://support.google.com/accounts/answer/185833#app-passwords&zippy=%2Cwhy-you-may-need-an-app-password
+sender_email_password = "asdfghjklpoiuytr" 
+sender_email = "your_email@some_domain.com"
+receiver_emails = [
+    "friend1@some_domain.com",
+    'friend2@some_domain.com',
+    'friend3@some_domain.com'
+    ]
